1fdfb3c97c643f0456c66393cab53a492d85b5ef
Mathivanan <mmathivanan17@gmail.com>
2019-11-11 20:00:39 +0530

    new update

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,6 +2,12 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		
+		int a = 15;
+		int b = 10;
+		int c;
+		c = a+b;
+		System.out.println(c);
 		System.out.println("Hello World");
 	}
 
